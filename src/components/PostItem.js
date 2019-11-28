dc4fa45b843b006919e89a2c291b3d074e913053
Fabiano de Matos Lopes <fabianomtlopes@yahoo.com.br>
2019-11-28 09:45:38 -0300

    desafio4

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function PostHeader ({author}){
+function PostHeader ({author, date}){
   return(
     <div className="postHeader">
       <img className="avatar" src={author.avatar} />
       <div className="details">
         <span>{author.name}</span>
-        <span>Teste2</span>
+        <span>{date}</span>
       </div>
     </div>
   );
 }
 
-function PostItem({author}){
+function PostComments({ comments }){
+  return(
+    <div className="postComment">
+      <div className="divider" />
+        { comments.map( comment => (
+          <div key={comment.id} className="comment">
+            <img className="avatar" src={comment.author.avatar} alt="Avatar do comentarista"/>
+            <p>
+              <span>{comment.author.name}</span>
+              {comment.content}
+            </p>
+          </div>
+        ))}
+      </div>
+  );
+}
+
+function PostItem({author, date, content, comments}){
   return(
     <div className="post">
-      <PostHeader author={author}/>
+      <PostHeader author={author} date={date} content={content}/>
+      <p className="postContent">{content}</p>
+      <PostComments comments={comments}/>
     </div>
   );
 }
 
 
-PostItem.PropTypes = {
-  author: PropTypes.string.isRequired,
+PostItem.propTypes = {
+  author: PropTypes.shape({
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+    }),
 }
 export default PostItem;
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -18,27 +18,70 @@ class PostList extends Component {
             id: 1,
             author: {
               name: "Diego Fernandes",
-              avatar: "https://i.pravatar.cc/150?img=58"
+              avatar: "https://i.pravatar.cc/150?img=60"
+            },
+            content:  ("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." )
+          }
+        ]
+      },
+
+      {
+        id: 2,
+        author: {
+          name: "Julio Alcantara",
+          avatar: "https://i.pravatar.cc/150?img=58"
+        },
+        date: "04 Jun 2019",
+        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Diego Fernandes",
+              avatar: "https://i.pravatar.cc/150?img=60"
             },
             content: "Conteúdo do comentário"
           }
         ]
       },
+
       {
-        id: 2
-        // Restante dos dados de um novo post
-      }
+        id: 3,
+        author: {
+          name: "Julio Alcantara",
+          avatar: "https://i.pravatar.cc/150?img=58"
+        },
+        date: "04 Jun 2019",
+        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        comments: [
+          {
+            id: 2,
+            author: {
+              name: "Diego Fernandes",
+              avatar: "https://i.pravatar.cc/150?img=60"
+            },
+            content: "Conteúdo do comentário"
+          },
+          {
+            id: 4,
+            author: {
+              name: 'Cézar Toledo',
+              avatar: 'https://i.pravatar.cc/150?img=11'
+            },
+            date: '04 Jun 2019',
+            content:
+              'Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes'
+          }
+        ]
+      },
     ]
   };
 
   render(){
-    const { posts } = this.state;
-
+    // const { posts } = this.state;
     return(
       <div className="postList">
-        {posts.map(post => (
-          <PostItem key={post.id} {...post} />
-        ))}
+        {this.state.posts.map(post => ( <PostItem key={post.id} {...post} />))}  
       </div>
     );
   }
@@ -48,3 +91,8 @@ class PostList extends Component {
 
 
 export default PostList;
+
+
+// {posts.map( post => (
+//   <PostItem key={post.id} {...post} /> 
+// ))},
